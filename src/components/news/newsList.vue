fd85588b29e40741b9e6e0d25e061d31c33a057f
dou <douyingzhe@qq.com>
2018-12-25 13:57:05 +0800

    新闻列表

---
+++ src/components/news/newsList.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h3>news list</h3>
+</div>
+</template>
+
+<script>
+  export default {
+    name: "newsList"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -3,13 +3,15 @@ import HomeContainer from './components/tabbar/HomeContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 import ShopCartContainer from './components/tabbar/ShopCartContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'//.vue后缀不可以省略
+import newsList from './components/news/newsList.vue'
 var router=new VueRouter({
   routes:[
     {path:'/',redirect:'/home'},
     {path:'/home',component:HomeContainer},
     {path:'/member',component:MemberContainer},
     {path:'/shopcart',component:ShopCartContainer},
-    {path:'/search',component:SearchContainer}
+    {path:'/search',component:SearchContainer},
+    {path:'/home/newsList',component:newsList}
   ],
 linkActiveClass:'mui-active'
 });
